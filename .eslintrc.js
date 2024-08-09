ebf95de5c66fca894d2c3ebc4b12124c45591c0f
yuu <yuu@users.noreply.github.com>
2024-08-09 11:07:52 +0900

    chore: install fundamental package

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: "next/core-web-vitals",
+  plugins: ["neverthrow"],
+  rules: {
+    "neverthrow/must-use-result": "error",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+    tsconfigRootDir: __dirname,
+  },
+};
