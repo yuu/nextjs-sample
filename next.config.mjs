004866e49d0f76683d0a2e39e73d4e4d69dff6fb
yuu <yuu@users.noreply.github.com>
2024-08-09 10:57:47 +0900

    chore: setup cloudscape

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: [
+    "@cloudscape-design/components",
+    "@cloudscape-design/component-toolkit",
+  ],
 };
 
 export default nextConfig;
