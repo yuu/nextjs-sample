e4766a398460e272d6060f382426e39f26fa25c8
yuu <yuu@users.noreply.github.com>
2024-08-12 14:31:56 +0900

    chore(test): impl form

---
+++ src/features/test/components/UserForm/UserForm.tsx
@@ -0,0 +1,196 @@
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  SpaceBetween,
+  Button,
+  Container,
+  Header,
+  FormField,
+  Input,
+} from "@cloudscape-design/components";
+import { TestCreateInputSchema } from "@/schema";
+import type { Prisma } from "@prisma/client";
+
+type UserFormProps<Data = any> = {
+  initialValue?: Data;
+  onSubmit: (valeus: Data) => PromiseLike<void>;
+};
+
+export const UserForm = ({ initialValue, onSubmit }: UserFormProps) => {
+  const { control, handleSubmit } = useForm<Prisma.TestCreateInput>({
+    resolver: zodResolver(TestCreateInputSchema),
+    defaultValues: initialValue,
+  });
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Form
+        actions={
+          <SpaceBetween direction="horizontal" size="xs">
+            <Button formAction="none" variant="link" href="/tests">
+              キャンセル
+            </Button>
+            <Button variant="primary">登録</Button>
+          </SpaceBetween>
+        }
+        header={<Header variant="h1">テスト登録</Header>}
+      >
+        <Container header={<Header variant="h2">テストフォーム</Header>}>
+          <SpaceBetween direction="vertical" size="l">
+            <Controller
+              name="familyName"
+              control={control}
+              render={({
+                field: { value, onChange },
+                fieldState: { error },
+              }) => (
+                <FormField label="氏" errorText={error?.message}>
+                  <Input
+                    autoFocus
+                    value={value}
+                    onChange={({ detail }) => onChange(detail.value)}
+                  />
+                </FormField>
+              )}
+            />
+
+            <Controller
+              name="firstName"
+              control={control}
+              render={({
+                field: { value, onChange },
+                fieldState: { error },
+              }) => (
+                <FormField label="名" errorText={error?.message}>
+                  <Input
+                    autoFocus
+                    value={value}
+                    onChange={({ detail }) => onChange(detail.value)}
+                  />
+                </FormField>
+              )}
+            />
+
+            <Controller
+              name="tel"
+              control={control}
+              render={({
+                field: { value, onChange },
+                fieldState: { error },
+              }) => (
+                <FormField label="TEL" errorText={error?.message}>
+                  <Input
+                    autoFocus
+                    value={value ?? ""}
+                    onChange={({ detail }) => onChange(detail.value)}
+                  />
+                </FormField>
+              )}
+            />
+
+            <Controller
+              name="email"
+              control={control}
+              render={({
+                field: { value, onChange },
+                fieldState: { error },
+              }) => (
+                <FormField label="メールアドレス" errorText={error?.message}>
+                  <Input
+                    placeholder="test@example.com"
+                    autoFocus
+                    inputMode="email"
+                    value={value}
+                    onChange={({ detail }) => onChange(detail.value)}
+                  />
+                </FormField>
+              )}
+            />
+
+            <Controller
+              name="postalCode"
+              control={control}
+              render={({
+                field: { value, onChange },
+                fieldState: { error },
+              }) => (
+                <FormField label="郵便番号" errorText={error?.message}>
+                  <Input
+                    autoFocus
+                    value={value ?? ""}
+                    onChange={({ detail }) => onChange(detail.value)}
+                  />
+                </FormField>
+              )}
+            />
+
+            <Controller
+              name="prefecture"
+              control={control}
+              render={({
+                field: { value, onChange },
+                fieldState: { error },
+              }) => (
+                <FormField label="都道府県" errorText={error?.message}>
+                  <Input
+                    value={value}
+                    onChange={({ detail }) => onChange(detail.value)}
+                  />
+                </FormField>
+              )}
+            />
+
+            <Controller
+              name="city"
+              control={control}
+              render={({
+                field: { value, onChange },
+                fieldState: { error },
+              }) => (
+                <FormField label="市町村" errorText={error?.message}>
+                  <Input
+                    value={value}
+                    onChange={({ detail }) => onChange(detail.value)}
+                  />
+                </FormField>
+              )}
+            />
+
+            <Controller
+              name="streetAddress"
+              control={control}
+              render={({
+                field: { value, onChange },
+                fieldState: { error },
+              }) => (
+                <FormField label="市町村以降" errorText={error?.message}>
+                  <Input
+                    value={value}
+                    onChange={({ detail }) => onChange(detail.value)}
+                  />
+                </FormField>
+              )}
+            />
+
+            <Controller
+              name="building"
+              control={control}
+              render={({
+                field: { value, onChange },
+                fieldState: { error },
+              }) => (
+                <FormField label="ビルや建物名" errorText={error?.message}>
+                  <Input
+                    value={value}
+                    onChange={({ detail }) => onChange(detail.value)}
+                  />
+                </FormField>
+              )}
+            />
+          </SpaceBetween>
+        </Container>
+      </Form>
+    </form>
+  );
+};
---
+++ src/pages/tests/new.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import { UserForm } from "@/features/test";
+import { trpc } from "@/api";
+
+const TestsNewPage: NextPage = () => {
+  const mutate = trpc.testCreate.useMutation();
+
+  const onSubmit = async (values: any) => {
+    const result = await mutate.mutate(values);
+
+    return Promise.resolve();
+  };
+
+  return <UserForm onSubmit={onSubmit} />;
+};
+
+export default TestsNewPage;
