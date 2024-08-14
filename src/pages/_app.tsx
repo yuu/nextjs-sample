cd06b592c84e2d15a66f7120bacb998a9549f537
yuu <yuu@users.noreply.github.com>
2024-08-15 00:09:28 +0900

    chore!: suppress react ssr warning

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { AppProps } from "next/app";
 import { trpc } from "@/api";
 import { AppLayout } from "@cloudscape-design/components";
@@ -9,6 +10,10 @@ import { useProgressBar } from "@/hooks";
 
 import "@cloudscape-design/global-styles/index.css";
 import "nprogress/nprogress.css";
+
+// Suppress warning for ssr useLayoutEffect
+// https://gist.github.com/gaearon/e7d97cdf38a2907924ea12e4ebdf3c85
+if (!process.browser) React.useLayoutEffect = React.useEffect;
 
 function App({ Component, pageProps }: AppProps) {
   useProgressBar();
