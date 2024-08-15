5423e2996618041b30df5e646fa8e4df4e233c43
yuu <yuu@users.noreply.github.com>
2024-08-15 23:13:49 +0900

    chore: setup react-intl

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,19 @@ const nextConfig = {
     "@cloudscape-design/components",
     "@cloudscape-design/component-toolkit",
   ],
+  i18n: {
+    locales: ["ja"],
+    defaultLocale: "ja",
+  },
+  webpack: (config, { dev, ...other }) => {
+    if (!dev) {
+      // https://formatjs.io/docs/guides/advanced-usage#react-intl-without-parser-40-smaller
+      config.resolve.alias["@formatjs/icu-messageformat-parser"] =
+        "@formatjs/icu-messageformat-parser/no-parser";
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import type { AppProps } from "next/app";
+import { IntlProvider } from "react-intl";
 import { trpc } from "@/api";
 import { AppLayout } from "@cloudscape-design/components";
 import { I18nProvider } from "@cloudscape-design/components/i18n";
@@ -7,6 +8,9 @@ import messages from "@cloudscape-design/components/i18n/messages/all.ja";
 import { Navigation } from "@/components/navigation";
 import { navigationItems } from "@/config/navigation";
 import { useProgressBar } from "@/hooks";
+
+// locales
+import i18nMessage from "@/locales/compiled/ja.json";
 
 import "@cloudscape-design/global-styles/index.css";
 import "nprogress/nprogress.css";
@@ -28,9 +32,16 @@ function App({ Component, pageProps }: AppProps) {
     ));
 
   return (
-    <I18nProvider locale={"ja"} messages={[messages]}>
-      {getLayout(<Component {...pageProps} />)}
-    </I18nProvider>
+    <IntlProvider
+      locale={"ja"}
+      timeZone="Asia/Tokyo"
+      defaultLocale="ja"
+      messages={i18nMessage}
+    >
+      <I18nProvider locale={"ja"} messages={[messages]}>
+        {getLayout(<Component {...pageProps} />)}
+      </I18nProvider>
+    </IntlProvider>
   );
 }
 
