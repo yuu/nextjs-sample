b2130ff9f5ad9564440579524bd2582f759f3f77
yuu <yuu@users.noreply.github.com>
2024-08-16 15:00:42 +0900

    chore(local): add default translate by rails

---
+++ src/locales/index.ts
@@ -0,0 +1,5 @@
+import { defaults } from "./translates/defaults";
+
+export const messages = {
+  defaults,
+};
---
+++ src/locales/translates/defaults.ts
@@ -0,0 +1,413 @@
+import { defineMessages } from "react-intl";
+
+const date = defineMessages({
+  abbr_day_names_mon: {
+    id: "defaults.date.abbr_day_names.mon",
+    defaultMessage: "月",
+  },
+  abbr_day_names_tue: {
+    id: "defaults.date.abbr_day_names.tue",
+    defaultMessage: "火",
+  },
+  abbr_day_names_wed: {
+    id: "defaults.date.abbr_day_names.wed",
+    defaultMessage: "水",
+  },
+  abbr_day_names_thu: {
+    id: "defaults.date.abbr_day_names.thu",
+    defaultMessage: "木",
+  },
+  abbr_day_names_fri: {
+    id: "defaults.date.abbr_day_names.fri",
+    defaultMessage: "金",
+  },
+  abbr_day_names_sat: {
+    id: "defaults.date.abbr_day_names.sat",
+    defaultMessage: "土",
+  },
+  abbr_day_names_sun: {
+    id: "defaults.date.abbr_day_names.sun",
+    defaultMessage: "日",
+  },
+  day_names_mon: {
+    id: "defaults.date.day_names.mon",
+    defaultMessage: "月曜日",
+  },
+  day_names_tue: {
+    id: "defaults.date.day_names.tue",
+    defaultMessage: "火曜日",
+  },
+  day_names_wed: {
+    id: "defaults.date.day_names.wed",
+    defaultMessage: "水曜日",
+  },
+  day_names_thu: {
+    id: "defaults.date.day_names.thu",
+    defaultMessage: "木曜日",
+  },
+  day_names_fri: {
+    id: "defaults.date.day_names.fri",
+    defaultMessage: "金曜日",
+  },
+  day_names_sat: {
+    id: "defaults.date.day_names.sat",
+    defaultMessage: "土曜日",
+  },
+  day_names_sun: {
+    id: "defaults.date.day_names.sun",
+    defaultMessage: "日曜日",
+  },
+  abbr_month_names_jan: {
+    id: "defaults.date.abbr_month_names.jan",
+    defaultMessage: "1月",
+  },
+  abbr_month_names_feb: {
+    id: "defaults.date.abbr_month_names.feb",
+    defaultMessage: "2月",
+  },
+  abbr_month_names_mar: {
+    id: "defaults.date.abbr_month_names.mar",
+    defaultMessage: "3月",
+  },
+  abbr_month_names_apr: {
+    id: "defaults.date.abbr_month_names.apr",
+    defaultMessage: "4月",
+  },
+  abbr_month_names_may: {
+    id: "defaults.date.abbr_month_names.may",
+    defaultMessage: "5月",
+  },
+  abbr_month_names_jun: {
+    id: "defaults.date.abbr_month_names.jun",
+    defaultMessage: "6月",
+  },
+  abbr_month_names_jul: {
+    id: "defaults.date.abbr_month_names.jul",
+    defaultMessage: "7月",
+  },
+  abbr_month_names_aug: {
+    id: "defaults.date.abbr_month_names.aug",
+    defaultMessage: "8月",
+  },
+  abbr_month_names_sep: {
+    id: "defaults.date.abbr_month_names.sep",
+    defaultMessage: "9月",
+  },
+  abbr_month_names_oct: {
+    id: "defaults.date.abbr_month_names.oct",
+    defaultMessage: "10月",
+  },
+  abbr_month_names_nov: {
+    id: "defaults.date.abbr_month_names.nov",
+    defaultMessage: "11月",
+  },
+  abbr_month_names_dec: {
+    id: "defaults.date.abbr_month_names.dec",
+    defaultMessage: "12月",
+  },
+  month_names_jan: {
+    id: "defaults.date.month_names.jan",
+    defaultMessage: "1月",
+  },
+  month_names_feb: {
+    id: "defaults.date.month_names.feb",
+    defaultMessage: "2月",
+  },
+  month_names_mar: {
+    id: "defaults.date.month_names.mar",
+    defaultMessage: "3月",
+  },
+  month_names_apr: {
+    id: "defaults.date.month_names.apr",
+    defaultMessage: "4月",
+  },
+  month_names_may: {
+    id: "defaults.date.month_names.may",
+    defaultMessage: "5月",
+  },
+  month_names_jun: {
+    id: "defaults.date.month_names.jun",
+    defaultMessage: "6月",
+  },
+  month_names_jul: {
+    id: "defaults.date.month_names.jul",
+    defaultMessage: "7月",
+  },
+  month_names_aug: {
+    id: "defaults.date.month_names.aug",
+    defaultMessage: "8月",
+  },
+  month_names_sep: {
+    id: "defaults.date.month_names.sep",
+    defaultMessage: "9月",
+  },
+  month_names_oct: {
+    id: "defaults.date.month_names.oct",
+    defaultMessage: "10月",
+  },
+  month_names_nov: {
+    id: "defaults.date.month_names.nov",
+    defaultMessage: "11月",
+  },
+  month_names_dec: {
+    id: "defaults.date.month_names.dec",
+    defaultMessage: "12月",
+  },
+});
+
+const datetime = defineMessages({
+  prompts_second: {
+    id: "defaults.datetime.prompts.second",
+    defaultMessage: "秒",
+  },
+  prompts_minute: {
+    id: "defaults.datetime.prompts.minute",
+    defaultMessage: "分",
+  },
+  prompts_hour: {
+    id: "defaults.datetime.prompts.hour",
+    defaultMessage: "時",
+  },
+  prompts_day: {
+    id: "defaults.datetime.prompts.day",
+    defaultMessage: "日",
+  },
+  prompts_month: {
+    id: "defaults.datetime.prompts.month",
+    defaultMessage: "月",
+  },
+  prompts_year: {
+    id: "defaults.datetime.prompts.year",
+    defaultMessage: "年",
+  },
+});
+
+const number = defineMessages({
+  human_storage_units_byte: {
+    id: "defaults.number.human.storage_units.byte",
+    defaultMessage: "バイト",
+  },
+  human_storage_units_kb: {
+    id: "defaults.number.human.storage_units.kb",
+    defaultMessage: "KB",
+  },
+  human_storage_units_mb: {
+    id: "defaults.number.human.storage_units.mb",
+    defaultMessage: "MB",
+  },
+  human_storage_units_gb: {
+    id: "defaults.number.human.storage_units.gb",
+    defaultMessage: "GB",
+  },
+  human_storage_units_tb: {
+    id: "defaults.number.human.storage_units.tb",
+    defaultMessage: "TB",
+  },
+  human_storage_units_pb: {
+    id: "defaults.number.human.storage_units.pb",
+    defaultMessage: "PB",
+  },
+  human_storage_units_eb: {
+    id: "defaults.number.human.storage_units.eb",
+    defaultMessage: "EB",
+  },
+});
+
+const helpers = defineMessages({
+  select_prompt: {
+    id: "defaults.helpers.select.prompt",
+    defaultMessage: "選択してください",
+  },
+  links_back: {
+    id: "defaults.helpers.links.back",
+    defaultMessage: "戻る",
+  },
+  links_cancel: {
+    id: "defaults.helpers.links.cancel",
+    defaultMessage: "キャンセル",
+  },
+  links_new: {
+    id: "defaults.helpers.links.new",
+    defaultMessage: "新規作成",
+  },
+  links_show: {
+    id: "defaults.helpers.links.show",
+    defaultMessage: "詳細",
+  },
+  links_edit: {
+    id: "defaults.helpers.links.edit",
+    defaultMessage: "編集",
+  },
+  links_create: {
+    id: "defaults.helpers.links.create",
+    defaultMessage: "登録",
+  },
+  links_update: {
+    id: "defaults.helpers.links.update",
+    defaultMessage: "更新",
+  },
+  links_destroy: {
+    id: "defaults.helpers.links.destroy",
+    defaultMessage: "削除",
+  },
+  submit_submit: {
+    id: "defaults.helpers.submit.submit",
+    defaultMessage: "保存する",
+  },
+  submit_create: {
+    id: "defaults.helpers.submit.create",
+    defaultMessage: "登録する",
+  },
+  submit_update: {
+    id: "defaults.helpers.submit.update",
+    defaultMessage: "更新する",
+  },
+  submit_destroy: {
+    id: "defaults.helpers.submit.destroy",
+    defaultMessage: "削除する",
+  },
+});
+
+const errors = defineMessages({
+  template_body: {
+    id: "defaults.errors.template.body",
+    defaultMessage: "次の項目を確認してください",
+  },
+  template_header_one: {
+    id: "defaults.errors.template.header.one",
+    defaultMessage: "{model}にエラーが発生しました",
+  },
+  template_header_other: {
+    id: "defaults.errors.template.header.other",
+    defaultMessage: "{model}に{count}個のエラーが発生しました",
+  },
+  record_not_destroyed: {
+    id: "defaults.errors.record_not_destroyed",
+    defaultMessage: "削除できませんでした",
+  },
+  already_linked: {
+    id: "defaults.errors.already_linked",
+    defaultMessage: "{model}はすでに紐づいています",
+  },
+  record_invalid: {
+    id: "defaults.errors.record_invalid",
+    defaultMessage: "バリデーションに失敗しました: {errors}",
+  },
+  restrict_dependent_destroy_has_many: {
+    id: "defaults.errors.restrict_dependent_destroy.has_many",
+    defaultMessage: "%{record}が存在しているので削除できません",
+  },
+  restrict_dependent_destroy_has_one: {
+    id: "defaults.errors.restrict_dependent_destroy.has_one",
+    defaultMessage: "%{record}が存在しているので削除できません",
+  },
+  fatal: {
+    id: "defaults.errors.messages.fatal",
+    defaultMessage:
+      "予期せぬエラーが発生しました。システム管理者にお問い合わせください",
+  },
+  model_invalid: {
+    id: "defaults.errors.messages.model_invalid",
+    defaultMessage: "バリデーションに失敗しました: {errors}",
+  },
+  confirmation: {
+    id: "defaults.errors.messages.confirmation",
+    defaultMessage: "{attr}と{attr_confirmation}の入力が一致しません",
+  },
+  accepted: {
+    id: "defaults.errors.messages.accepted",
+    defaultMessage: "{attr}を受諾してください",
+  },
+  blank: {
+    id: "defaults.errors.messages.blank",
+    defaultMessage: "{attr}を入力してください",
+  },
+  empty: {
+    id: "defaults.errors.messages.empty",
+    defaultMessage: "{attr}を入力してください",
+  },
+  even: {
+    id: "defaults.errors.messages.even",
+    defaultMessage: "{attr}は偶数にしてください",
+  },
+  odd: {
+    id: "defaults.errors.messages.odd",
+    defaultMessage: "{attr}は奇数にしてください",
+  },
+  exclusion: {
+    id: "defaults.errors.messages.exclusion",
+    defaultMessage: "{attr}は予約されています",
+  },
+  inclusion: {
+    id: "defaults.errors.messages.inclusion",
+    defaultMessage: "{attr}は一覧にありません",
+  },
+  invalid: {
+    id: "defaults.errors.messages.invalid",
+    defaultMessage: "{attr}は不正な値です",
+  },
+  equal_to: {
+    id: "defaults.errors.messages.equal_to",
+    defaultMessage: "{attr}は{count}にしてください",
+  },
+  greater_than: {
+    id: "defaults.errors.messages.greater_than",
+    defaultMessage: "{attr}は{count}より大きい値にしてください",
+  },
+  greater_than_or_equal_to: {
+    id: "defaults.errors.messages.greater_than_or_equal_to",
+    defaultMessage: "{attr}は{count}以上の値にしてください",
+  },
+  less_than: {
+    id: "defaults.errors.messages.less_than",
+    defaultMessage: "{attr}は{count}より小さい値にしてください",
+  },
+  less_than_or_equal_to: {
+    id: "defaults.errors.messages.less_than_or_equal_to",
+    defaultMessage: "{attr}は{count}以下の値にしてください",
+  },
+  not_a_number: {
+    id: "defaults.errors.messages.not_a_number",
+    defaultMessage: "{attr}は数値で入力してください",
+  },
+  not_an_integer: {
+    id: "defaults.errors.messages.not_an_integer",
+    defaultMessage: "{attr}は整数で入力してください",
+  },
+  other_than: {
+    id: "defaults.errors.messages.other_than",
+    defaultMessage: "{attr}は{count}以外の値にしてください",
+  },
+  present: {
+    id: "defaults.errors.messages.present",
+    defaultMessage: "{attr}は入力しないでください",
+  },
+  required: {
+    id: "defaults.errors.messages.required",
+    defaultMessage: "{attr}を入力してください",
+  },
+  taken: {
+    id: "defaults.errors.messages.taken",
+    defaultMessage: "{attr}はすでに存在します",
+  },
+  too_long: {
+    id: "defaults.errors.messages.too_long",
+    defaultMessage: "{attr}は{count}文字以内で入力してください",
+  },
+  too_short: {
+    id: "defaults.errors.messages.too_short",
+    defaultMessage: "{attr}は{count}文字以上で入力してください",
+  },
+  wrong_length: {
+    id: "defaults.errors.messages.wrong_length",
+    defaultMessage: "{attr}は{count}文字で入力してください",
+  },
+});
+
+export const defaults = {
+  date,
+  datetime,
+  number,
+  helpers,
+  errors,
+};
