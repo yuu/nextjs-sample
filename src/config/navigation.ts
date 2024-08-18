9368480138a7ed5f409ddc17a463ecdfdf449a88
yuu <yuu@users.noreply.github.com>
2024-08-18 14:59:21 +0900

    fix: fix logout menu

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -1,6 +1,7 @@
 import {
   type SideNavigationProps,
   type TopNavigationProps,
+  type ButtonDropdownProps,
 } from "@cloudscape-design/components";
 
 export const sideNavigationItems =
@@ -29,71 +30,23 @@ export const identity = (): TopNavigationProps.Identity => {
   };
 };
 
-export const utilities = (): Array<TopNavigationProps.Utility> => {
+type UtilitiesParams = {
+  onItemClick: (
+    event: CustomEvent<ButtonDropdownProps.ItemClickDetails>
+  ) => void;
+};
+
+export const utilities = ({
+  onItemClick,
+}: UtilitiesParams): Array<TopNavigationProps.Utility> => {
   return [
     {
-      type: "button",
-      text: "Link",
-      href: "https://example.com/",
-      external: true,
-      externalIconAriaLabel: " (opens in a new tab)",
-    },
-    {
-      type: "button",
-      iconName: "notification",
-      title: "Notifications",
-      ariaLabel: "Notifications (unread)",
-      badge: true,
-      disableUtilityCollapse: false,
-    },
-    {
       type: "menu-dropdown",
-      iconName: "settings",
-      ariaLabel: "Settings",
-      title: "Settings",
-      items: [
-        {
-          id: "settings-org",
-          text: "Organizational settings",
-        },
-        {
-          id: "settings-project",
-          text: "Project settings",
-        },
-      ],
-    },
-    {
-      type: "menu-dropdown",
-      text: "Customer Name",
-      description: "email@example.com",
+      text: " ",
+      description: " ",
       iconName: "user-profile",
-      items: [
-        { id: "profile", text: "Profile" },
-        { id: "preferences", text: "Preferences" },
-        { id: "security", text: "Security" },
-        {
-          id: "support-group",
-          text: "Support",
-          items: [
-            {
-              id: "documentation",
-              text: "Documentation",
-              href: "#",
-              external: true,
-              externalIconAriaLabel: " (opens in new tab)",
-            },
-            { id: "support", text: "Support" },
-            {
-              id: "feedback",
-              text: "Feedback",
-              href: "#",
-              external: true,
-              externalIconAriaLabel: " (opens in new tab)",
-            },
-          ],
-        },
-        { id: "signout", text: "Sign out" },
-      ],
+      items: [{ id: "signout", text: "ログアウト" }],
+      onItemClick: onItemClick,
     },
   ];
 };
--- src/layout/DefaultLayout/DefaultLayout.tsx
+++ src/layout/DefaultLayout/DefaultLayout.tsx
@@ -1,20 +1,43 @@
+import { match } from "ts-pattern";
 import { ReactNode } from "react";
 import { AppLayout } from "@cloudscape-design/components";
+import { type ButtonDropdownProps } from "@cloudscape-design/components";
 import { SideNavigation, TopNavigation } from "@/components/navigation";
 import { sideNavigationItems, identity, utilities } from "@/config/navigation";
+import { useAuthContext } from "@/context";
 
 type DefaultLayoutProps = {
   content: ReactNode;
 };
 
+const useAction = () => {
+  const auth = useAuthContext();
+  const onItemClick = (
+    event: CustomEvent<ButtonDropdownProps.ItemClickDetails>
+  ) => {
+    const f = match(event.detail.id)
+      .with("signout", () => () => auth.signout())
+      .otherwise((v) => () => console.log("no implement id:", v));
+
+    f();
+  };
+
+  return { onItemClick };
+};
+
 export const DefaultLayout = ({ content }: DefaultLayoutProps) => {
+  const { onItemClick } = useAction();
+
   return (
     <>
       <div
         id="top-navigation-wrapper"
         style={{ position: "sticky", top: 0, zIndex: 1002 }}
       >
-        <TopNavigation identity={identity()} utilities={utilities()} />
+        <TopNavigation
+          identity={identity()}
+          utilities={utilities({ onItemClick })}
+        />
       </div>
       <AppLayout
         headerSelector="#top-navigation-wrapper"
